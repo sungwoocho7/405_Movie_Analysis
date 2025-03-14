ed3d826de17a635b7c4c237ba93d127b44ad5dd8
Ubuntu <ubuntu@ip-172-31-30-44.ec2.internal>
2025-03-14 00:35:24 +0000

    Added DuckDB query file

---
+++ duckdb/duckdb_query.sql
@@ -0,0 +1,28 @@
+WITH MovieData AS (
+
+    SELECT 
+        title,
+        director,
+        genres,
+        vote_average,
+        revenue,
+        budget,
+        release_date,
+        EXTRACT(YEAR FROM release_date) AS release_year,       
+	EXTRACT(MONTH FROM release_date) AS release_month
+    FROM read_csv_auto('/mnt/data/movies_cleaned.csv', HEADER=TRUE)
+    WHERE revenue > 0  
+)
+
+SELECT 
+    release_year,
+    genres,
+    COUNT(title) AS movie_count,
+    SUM(revenue) AS total_revenue,
+    AVG(revenue) AS avg_movie_revenue,
+    AVG(vote_average) AS avg_movie_rating
+FROM MovieData
+WHERE release_year IS NOT NULL AND genres IS NOT NULL
+GROUP BY release_year, genres
+ORDER BY release_year DESC;
+
